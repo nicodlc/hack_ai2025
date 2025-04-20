ffae8c9640c532ea5bc5fbb20f97ffa5eeac3263
Nicolas De la Cruz <nico.dlcv@gmail.com>
2025-04-19 21:42:44 -0500

    updated database controller

--- database/dbController.py
+++ database/dbController.py
@@ -1,4 +1,18 @@
 import openai
 import ollama
 import astrapy
+import json
+from langchain_text_splitters import MarkdownTextSplitter, RecursiveCharacterTextSplitter
+from llama_index.core import SimpleDirectoryReader
 
+class ProcessDocuments: 
+    def __init__(self):
+        self.markdown_dir = "./data"
+
+        pass
+    
+    def createDocs(self, something):
+        pass
+
+    def embedd(self, text: str):
+        pass
